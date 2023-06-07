eecb64a64d8c6828186e48fcbaa2cd49a7e7f75c
Alex Sederquest <alexsederquest98@gmail.com>
2023-06-07 16:50:43 -0400

    Updated includes in Quasar.h for use in applications using the engine

---
+++ Quasar/src/Quasar.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// For use by Quasar applications
+#include "Quasar/Core/Base.h"
+
+#include "Quasar/Core/Application.h"
+#include "Quasar/Core/Logger.h"
+
+#include "Quasar/Core/KeyCodes.h"
+#include "Quasar/Core/MouseCodes.h"
+
+// ---------- ENTRY POINT ----------
+#include "Quasar/Core/EntryPoint.h"
