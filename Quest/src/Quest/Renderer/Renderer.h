c01931d80f65ff141a7e0adc84f61f32a43c25f6
Alex Sederquest <alexsederquest98@gmail.com>
2023-07-01 18:04:00 -0400

    Basic Renderer.h

--- Quest/src/Quest/Renderer/Renderer.h
+++ Quest/src/Quest/Renderer/Renderer.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "Quest/Core/Base.h"
+#include "Quest/Renderer/RendererAPI.h"
+
+namespace Quest
+{
+	class Renderer
+	{
+	public:
+		static void Init();
+		static void Shutdown();
+
+		static void OnWindowResize(uint32_t width, uint32_t height);
+
+		static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
+	private:
+	};
+}
