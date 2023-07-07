1b67ea43ac24cf45e7ced83faeb9d47ecce1c850
Alex Sederquest <alexsederquest98@gmail.com>
2023-07-07 19:02:35 -0400

    Updated names on some functions in Shader.h

--- Quest/src/Quest/Renderer/Shader.h
+++ Quest/src/Quest/Renderer/Shader.h
@@ -15,9 +15,9 @@ namespace Quest
 
 		virtual void SetInt(const uint32_t& id, int value) = 0;
 		virtual void SetFloat(const uint32_t& id, float value) = 0;
-		virtual void SetFloat2(const uint32_t& id, const glm::vec2& value) = 0;
-		virtual void SetFloat3(const uint32_t& id, const glm::vec3& value) = 0;
-		virtual void SetFloat4(const uint32_t& id, const glm::vec4& value) = 0;
+		virtual void SetVec2(const uint32_t& id, const glm::vec2& value) = 0;
+		virtual void SetVec3(const uint32_t& id, const glm::vec3& value) = 0;
+		virtual void SetVec4(const uint32_t& id, const glm::vec4& value) = 0;
 		virtual void SetMat4(const uint32_t& id, const glm::mat4& value) = 0;
 
 		virtual const uint32_t GetID() const = 0;
