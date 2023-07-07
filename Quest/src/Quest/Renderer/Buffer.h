083aaf4a5a9f832fdda97b7576d3688f840125e7
Alex Sederquest <alexsederquest98@gmail.com>
2023-07-07 19:19:54 -0400

    Added a BufferLayout to Buffer.h as well as the opengl implementation of Buffer.

--- Quest/src/Quest/Renderer/Buffer.h
+++ Quest/src/Quest/Renderer/Buffer.h
@@ -2,6 +2,73 @@
 
 namespace Quest
 {
+	enum class ShaderDataType
+	{
+		None = 0, Float, Vec2, Vec3, Vec4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+	};
+
+	static uint32_t GetShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:		return 4;
+		case ShaderDataType::Vec2:		return 4 * 2;
+		case ShaderDataType::Vec3:		return 4 * 3;
+		case ShaderDataType::Vec4:		return 4 * 4;
+		case ShaderDataType::Mat3:		return 4 * 3 * 3;
+		case ShaderDataType::Mat4:		return 4 * 4 * 4;
+		case ShaderDataType::Int:		return 4;
+		case ShaderDataType::Int2:		return 4 * 2;
+		case ShaderDataType::Int3:		return 4 * 3;
+		case ShaderDataType::Int4:		return 4 * 4;
+		case ShaderDataType::Bool:		return 1;
+		}
+		
+		QE_CORE_ASSERT(false, "Invalid ShaderDataType");
+		return 0;
+	}
+
+	struct BufferElement
+	{
+		std::string Name;
+		uint32_t Size;
+		uint32_t Offset;
+		ShaderDataType DataType;
+
+		BufferElement(ShaderDataType type, const std::string& name)
+			: Name(name), DataType(type), Size(GetShaderDataTypeSize(type)), Offset(0)
+		{
+		}
+	};
+
+	class BufferLayout
+	{
+	public:
+		BufferLayout() {}
+		BufferLayout(std::initializer_list<BufferElement> elements)
+			: m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+
+		const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+	private:
+		void CalculateOffsetsAndStride()
+		{
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements)
+			{
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
 	class VertexBuffer
 	{
 	public:
@@ -10,6 +77,9 @@ namespace Quest
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
 		//virtual void SetData();
+
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
 
 		static Ref<VertexBuffer> Create(uint32_t size);
 		static Ref<VertexBuffer> Create(float* vertices, uint32_t size);
